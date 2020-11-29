9f5f98023beaa1656b7805ced29d47b128f484e4
Fadi Nicolas Zahhar <54265928+fadiogilvy@users.noreply.github.com>
2020-11-29 03:28:22 +0200

    adding filters to the solution

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,5 @@
 import React, {useState,useEffect, useCallback} from 'react';
-import { PageHeader, Button  } from 'antd';
+import { PageHeader, Button, DatePicker, Input  } from 'antd';
 import PostSnippet from './PostSnippet';
 //import api from '../mock_api';
 import _ from 'lodash';
@@ -11,10 +11,55 @@ const Posts = (props) => {
     const [posts, setPosts] = useState([])
     const [limit, setLimit] = useState(5);
     const [morePosts, setMorePosts] = useState(false);
+    const [title, setTitle] = useState('');
 
 
     function getCollection(userId,slug) {
         let collection = db.collection('users').doc(userId).collection('posts')
+                if (slug != "all") {
+            collection = collection.where('material',"==",slug)
+        }
+
+            collection.limit(limit).onSnapshot(async posts => { // allows to subscribes
+                let postsData = await posts.docs.map(post => {
+                    let data = post.data()
+                    let { id } = post
+                    let payload = {
+                        id,
+                        ...data
+                    }
+                    return payload
+                });
+                setMorePosts(posts.docs.length >= limit)
+                setPosts(postsData)
+            }) // end on snapshot
+    }
+
+
+    function filterCollection(userId,slug,materialDate) {
+        let collection = db.collection('users').doc(userId).collection('posts').where('materialDate', "==", materialDate)
+                if (slug != "all") {
+            collection = collection.where('material',"==",slug)
+        }
+
+            collection.limit(limit).onSnapshot(async posts => { // allows to subscribes
+                let postsData = await posts.docs.map(post => {
+                    let data = post.data()
+                    let { id } = post
+                    let payload = {
+                        id,
+                        ...data
+                    }
+                    return payload
+                });
+                setMorePosts(posts.docs.length >= limit)
+                setPosts(postsData)
+            }) // end on snapshot
+    }
+
+
+    function filterWord(userId,slug,word) {
+        let collection = db.collection('users').doc(userId).collection('posts').where('title','==',word);
                 if (slug != "all") {
             collection = collection.where('material',"==",slug)
         }
@@ -53,16 +98,43 @@ const Posts = (props) => {
 
     } // update the callback if the state changes
 
-    const onPrevious = (event) => {
 
-        if (limit >= 5) {
-            let userId = props?.user.uid ? props?.user.uid : props.uid
+    function formatDate(date) {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+    
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
+    
+        return [year, month, day].join('-');
+    }
+
+    const onDateChange = (event) => {
+
+        let userId = props?.user.uid ? props?.user.uid : props.uid
         let slug = props?.slug ? props.slug : "all"
-        setLimit(limit-5);
-        getCollection(userId,slug)
-        }
+        filterCollection(userId,slug,formatDate(event))
+        
 
     }
+
+
+    const onSearchChange = (event) => {
+        setTitle(event.target.value);
+    }
+    const onSearch = (event) => {
+
+        let userId = props?.user.uid ? props?.user.uid : props.uid
+        let slug = props?.slug ? props.slug : "all"
+        filterWord(userId,slug,title)
+        
+
+    }
+
     return (
         <div className="posts_container">
             <div className="page_header_container">
@@ -72,7 +144,10 @@ const Posts = (props) => {
                 }}
                 title="All Materials"
                 />
-
+<h2>Choose a date to filter or search a word</h2>
+<DatePicker size="large" onChange={onDateChange} /> <Input id="serch" name="search" placeholder="Search a word" value={title} onChange={onSearchChange}  style={{
+                        width: '300px',
+                    }}/> <button onClick={onSearch}>Search</button>
             </div>
 
             <div className="articles_container">
